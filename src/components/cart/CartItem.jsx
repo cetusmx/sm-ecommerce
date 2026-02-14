fe73382fcf4e08dbfca504f5d8398bc1848edf17
Oscar Rodriguez <rodroso@gmail.com>
2026-02-14 17:15:52 -0600

    2.95

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -15,7 +15,7 @@ const CartItem = ({ item, deliveryInfo }) => {
   }
 
   // Determine the correct image URL based on category
-  const perfilesUrl = `/Perfiles/${item.perfil}.jpg`;
+  const perfilesUrl = `/Perfiles/${item.perfil}.png`;
   const sugeridosUrl = `/Sugeridos/${item.clave}.jpg`;
   const imageUrl = (item.categoria === 'Herramientas' || item.categoria === 'Accesorios' || item.categoria === 'Estuches' || item.categoria === 'Accesorios hidráulicos')
     ? sugeridosUrl
