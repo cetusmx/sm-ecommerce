df4c391f69a62a6f6ebd23e1076e77ec46e24017
Oscar Rodriguez <rodroso@gmail.com>
2026-02-14 03:54:51 -0600

    2.89

--- src/components/common/TopBanner.jsx
+++ src/components/common/TopBanner.jsx
@@ -3,7 +3,7 @@ import styles from './TopBanner.module.css';
 
 const messages = [
   "*** Paga con tarjeta o transferencia ***",
-  "Obtén envío gratis en compras mayores a $500"
+  "--> Envío gratis por tiempo limitado <--"
 ];
 
 const TopBanner = () => {
